e8bc917a706d4226709fbc647a0b33892919e34b
Timm Preetz <timm@preetz.us>
2024-10-15 20:14:12 +0200

    Add more query methods

--- lib/src/index_column.dart
+++ lib/src/index_column.dart
@@ -32,17 +32,22 @@ class IndexColumn<T /* entity type */, I /* index type */ > {
 
   final I Function(T e) _getIndexValueFunc;
 
-  // Usually I, just for `DateTime` we have some special handling to support that out of the box (by converting to String)
+  // Usually I, just for `DateTime` we have some special handling to support that out of the box (by converting to int)
   dynamic _getIndexValue(T e) {
     final v = _getIndexValueFunc(e);
     if (v is DateTime) {
-      return v.toIso8601String();
+      return v.microsecondsSinceEpoch;
     }
 
     return v;
   }
 
+  // NOTE(tp): Parameters here are typed as `dynamic`, even though they must be `I`. This is done so we can throw a more detailed exeption instead of the default low-level `TypeError`
   Query equals(dynamic value) {
+    if (value is int && I == double) {
+      value = value.toDouble();
+    }
+
     if (value is! I) {
       throw Exception(
         'Can not build query as field "$_field" needs a value of type $I, but got ${value.runtimeType}.',
@@ -52,9 +57,103 @@ class IndexColumn<T /* entity type */, I /* index type */ > {
     return _EqualQuery(
       _entity,
       _field,
-      value is DateTime ? value.toIso8601String() : value,
+      value,
     );
   }
+
+  Query greaterThan(dynamic value) {
+    if (value is int && I == double) {
+      value = value.toDouble();
+    }
+
+    if (value is! I) {
+      throw Exception(
+        'Can not build query as field "$_field" needs a value of type $I, but got ${value.runtimeType}.',
+      );
+    }
+
+    return _GreaterThanQuery(
+      _entity,
+      _field,
+      value,
+    );
+  }
+
+  // Query operator >(I value) {
+  //   return greaterThan(value);
+  // }
+
+  Query greaterThanOrEqual(dynamic value) {
+    if (value is int && I == double) {
+      value = value.toDouble();
+    }
+
+    if (value is! I) {
+      throw Exception(
+        'Can not build query as field "$_field" needs a value of type $I, but got ${value.runtimeType}.',
+      );
+    }
+
+    return _OrQuery(
+      equals(value),
+      _GreaterThanQuery(
+        _entity,
+        _field,
+        value,
+      ),
+    );
+  }
+
+  // Query operator >=(I value) {
+  //   return greaterThanOrEqual(value);
+  // }
+
+  Query lessThan(dynamic value) {
+    if (value is int && I == double) {
+      value = value.toDouble();
+    }
+
+    if (value is! I) {
+      throw Exception(
+        'Can not build query as field "$_field" needs a value of type $I, but got ${value.runtimeType}.',
+      );
+    }
+
+    return _LessThanQuery(
+      _entity,
+      _field,
+      value,
+    );
+  }
+
+  // Query operator <(I value) {
+  //   return lessThan(value);
+  // }
+
+  Query lessThanOrEqual(dynamic value) {
+    if (value is int && I == double) {
+      value = value.toDouble();
+    }
+
+    if (value is! I) {
+      throw Exception(
+        'Can not build query as field "$_field" needs a value of type $I, but got ${value.runtimeType}.',
+      );
+    }
+
+    return _OrQuery(
+      equals(value),
+      _LessThanQuery(
+        _entity,
+        _field,
+        value,
+      ),
+    );
+  }
+
+  // Query operator <=(I value) {
+  //   return lessThanOrEqual(value);
+  // }
 }
 
 bool _typeEqual<T, Y>() => T == Y;
