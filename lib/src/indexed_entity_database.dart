058564857d87478e7eac3f5ebf0a05fd6a15f607
Timm Preetz <timm@preetz.us>
2024-10-31 16:09:12 +0100

    Add `insertMany` method

--- lib/src/indexed_entity_database.dart
+++ lib/src/indexed_entity_database.dart
@@ -10,34 +10,62 @@ class IndexedEntityDabase {
       "SELECT name FROM sqlite_master WHERE type='table' AND name='entity';",
     );
     if (res.isEmpty) {
-      _database.execute('PRAGMA foreign_keys = ON');
+      debugPrint('Creating new DB');
 
-      _database.execute(
-        'CREATE TABLE `entity` ( `type` TEXT NOT NULL, `key` NOT NULL, `value`, PRIMARY KEY ( `type`, `key` ) )',
-      );
+      _initialDBSetup();
+      _v2Migration();
+    } else if (_dbVersion == 1) {
+      debugPrint('Migrating DB to v2');
 
-      _database.execute(
-        'CREATE TABLE `index` ( `type` TEXT NOT NULL, `entity` NOT NULL, `field` TEXT NOT NULL, `value`, '
-        ' FOREIGN KEY (`type`, `entity`) REFERENCES `entity` (`type`, `key`) ON DELETE CASCADE'
-        ')',
-      );
-
-      _database.execute(
-        'CREATE INDEX index_field_values '
-        'ON `index` ( `type`, `field`, `value` )',
-      );
-
-      _database.execute(
-        'CREATE TABLE `metadata` ( `key` TEXT NOT NULL, `value` )',
-      );
-
-      _database.execute(
-        'INSERT INTO `metadata` ( `key`, `value` ) VALUES ( ?, ? )',
-        ['version', 1],
-      );
-
-      debugPrint('New DB created');
+      _v2Migration();
     }
+
+    assert(_dbVersion == 2);
+  }
+
+  void _initialDBSetup() {
+    _database.execute('PRAGMA foreign_keys = ON');
+
+    _database.execute(
+      'CREATE TABLE `entity` ( `type` TEXT NOT NULL, `key` NOT NULL, `value`, PRIMARY KEY ( `type`, `key` ) )',
+    );
+
+    _database.execute(
+      'CREATE TABLE `index` ( `type` TEXT NOT NULL, `entity` NOT NULL, `field` TEXT NOT NULL, `value`, '
+      ' FOREIGN KEY (`type`, `entity`) REFERENCES `entity` (`type`, `key`) ON DELETE CASCADE'
+      ')',
+    );
+
+    _database.execute(
+      'CREATE INDEX index_field_values '
+      'ON `index` ( `type`, `field`, `value` )',
+    );
+
+    _database.execute(
+      'CREATE TABLE `metadata` ( `key` TEXT NOT NULL, `value` )',
+    );
+
+    _database.execute(
+      'INSERT INTO `metadata` ( `key`, `value` ) VALUES ( ?, ? )',
+      ['version', 1],
+    );
+  }
+
+  int get _dbVersion => _database.select(
+        'SELECT `value` FROM `metadata` WHERE `key` = ?',
+        ['version'],
+      ).single['value'] as int;
+
+  void _v2Migration() {
+    _database.execute(
+      'CREATE UNIQUE INDEX index_type_entity_field_index '
+      'ON `index` ( `type`, `entity`, `field` )',
+    );
+
+    _database.execute(
+      'UPDATE `metadata` SET `value` = ? WHERE `key` = ?',
+      [2, 'version'],
+    );
   }
 
   factory IndexedEntityDabase.open(String path) {
